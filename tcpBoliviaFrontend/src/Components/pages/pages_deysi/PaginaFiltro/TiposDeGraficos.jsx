985f6140a4de0556d5ca9e9c3255a46d6030511e
Deysi <deysymancilla123@gmail.com>
2024-12-17 18:31:41 -0400

    first commit

---
+++ tcpBoliviaFrontend/src/Components/pages/pages_deysi/PaginaFiltro/TiposDeGraficos.jsx
@@ -0,0 +1,19 @@
+const TiposDeGraficos = ({ viewType, chartType, setChartType }) => {
+    return (
+        <>
+            {viewType === 'chart' ? (
+                <select
+                    onChange={(e) => setChartType(e.target.value)}
+                    value={chartType}
+                    className="bg-slate-100 p-3"
+                >
+                    <option value="bar">Gráfico de Barras</option>
+                    <option value="line">Gráfico de Líneas</option>
+                    <option value="pie">Gráfico Circular</option>
+                </select>
+            ) : null}
+        </>
+    )
+}
+
+export default TiposDeGraficos
